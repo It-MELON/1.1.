1125f4cff007de2b7ed1bb7e258c77356e5a9147
It-MELON <Dani.golol.2007.com@gmail.com>
2025-11-21 11:49:09 +0700

    first commit

---
+++ catalog/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
+
+admin.site.register(Language)
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        ('About book', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
+
+class BooksInstanceInline(admin.StackedInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+class BookInline(admin.StackedInline):
+    model = Book
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Name', {
+            'fields': ('first_name', 'last_name')
+        }),
+        ('Dates', {
+            'fields': ('date_of_birth', 'date_of_death')
+        })
+    )
+    inlines = [BookInline]
+
+admin.site.register(Genre)
